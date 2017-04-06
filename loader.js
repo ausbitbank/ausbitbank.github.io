332e9b4ba9ba4c5d9bdf3abbab300238ae7c0828
ausbitbank <ausbitbank@users.noreply.github.com>
2017-04-06 14:42:54 +1000

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,8 +1,6 @@
 function loadScript(url, callback){
-
     var script = document.createElement("script")
     script.type = "text/javascript";
-
     if (script.readyState){  //IE
         script.onreadystatechange = function(){
             if (script.readyState == "loaded" ||
@@ -16,7 +14,6 @@ function loadScript(url, callback){
             callback();
         };
     }
-
     script.src = url;
     document.getElementsByTagName("head")[0].appendChild(script);
 }
